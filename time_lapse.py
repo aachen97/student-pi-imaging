a4be2536bfce23531aaa629173ded42aba891671
Dylan Pham <43.dpham@gmail.com>
2019-07-23 19:19:04 +0000

    comments

--- time_lapse.py
+++ time_lapse.py
@@ -10,17 +10,19 @@ import time
 time_at_start = time.time()
 
 # the path where the raspberry pi will store its file
-RASPI_PATH = f"/home/raspberrypi/{time_at_start}"
+RASPI_PATH = f"/home/raspberrypi/{time_at_start}/"
 
-# time between captures
+# seconds between captures
 INTERVAL = 1
-SESSION_LENGTH = 60
+# seconds in the air
+SESSION_LENGTH = 300
 
 with PiCamera() as camera:
+    # preview on pi screen
     camera.start_preview()
 
     try:
-        for filename in camera.capture_continuous(RASPI_PATH + '/image{timestamp}.png'):
+        for filename in camera.capture_continuous(RASPI_PATH + 'image{timestamp}.png'):
             time.sleep(INTERVAL)
 
             # time.time() is time at this point of the script
