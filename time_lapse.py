be1a85a2909a111bceb6fcbb3f432352f47703b7
Dylan Pham <43.dpham@gmail.com>
2019-07-23 19:07:56 +0000

    comments + fixing variable error

--- time_lapse.py
+++ time_lapse.py
@@ -6,23 +6,25 @@ from picamera import PiCamera
 from time import sleep
 import time
 
-# the path where the raspberry pi will store its file
-RASPI_PATH = f"/home/raspberrypi/{time.time()}"
+# time at start of script
+time_at_start = time.time()
 
-INTERVAL = 1 # the time interval (in seconds) between pictures
-SESSION_LENGTH = 60 # the duration of the script (in seconds)
-time_at_start = time.time() # time at start of script
+# the path where the raspberry pi will store its file
+RASPI_PATH = f"/home/raspberrypi/{time_at_start}"
+
+# time between captures
+INTERVAL = 1
+SESSION_LENGTH = 60
 
 with PiCamera() as camera:
     camera.start_preview()
 
     try:
         for filename in camera.capture_continuous(RASPI_PATH + '/image{timestamp}.png'):
-            print(filename)
             time.sleep(INTERVAL)
 
             # time.time() is time at this point of the script
-            if time.time() - seconds_passed >= SESSION_LENGTH:
+            if time.time() - time_at_start >= SESSION_LENGTH:
                 break
     finally:
         camera.stop_preview()
